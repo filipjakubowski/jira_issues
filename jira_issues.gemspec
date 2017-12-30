d1b50d687343ec3611292af5248dd8868c66f655
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2017-12-30 22:39:20 +0000

    workflow fix

--- jira_issues.gemspec
+++ jira_issues.gemspec
@@ -3,7 +3,7 @@ $:.push File.expand_path("../lib", __FILE__)
 require "jira_issues/version"
 
 Gem::Specification.new do |s|
-  s.name        = 'jira-issues'
+  s.name        = 'jira_issues'
   s.version     = JiraIssues::VERSION
   s.licenses    = ['MIT']
 
@@ -18,10 +18,10 @@ Gem::Specification.new do |s|
 
   s.rubyforge_project = "jira_issues"
 
-  s.require_paths = %w[lib]
-  #s.files         = Dir['lib/*.rb']  
   s.files          = %w{lib/jira_issues.rb lib/jira_issues/jira_adapter.rb lib/jira_issues/jira_issue_mapper.rb lib/jira_issues/jira_issues_navigator.rb lib/jira_issues/jira_workflow.rb lib/jira_issues/jira_query.rb lib/jira_issues/jira_issues_stats.rb lib/jira_issues/jql_lib.rb}
 
+  s.require_paths = %w[lib]
+  
   s.add_dependency 'jira-ruby', '~>1.4'
 
   ## List your development dependencies here. Development dependencies are
--- lib/jira_issues/jira_workflow.rb
+++ lib/jira_issues/jira_workflow.rb
@@ -14,13 +14,19 @@ module JiraIssues
 
     def self.issue_status_for_name(project_key, status_name)
       {
-        'Backlog'     => OPEN,
-        'Open'        => OPEN,
-        'Blocked'     => BLOCKED,
-        'In Progress' => IN_PROGRESS,
-        'Done'        => CLOSED,
-        'Closed'      => CLOSED
+        'Backlog'       => OPEN,
+        'Open'          => OPEN,
+        'To Do'         => OPEN,
+        'Blocked'       => BLOCKED,
+        'In Progress'   => IN_PROGRESS,
+        'Specification' => IN_PROGRESS,
+        'Done'          => CLOSED,
+        'Closed'        => CLOSED,
+        'Resolved'      => CLOSED,
       }.fetch(status_name)
+    rescue KeyError => e
+      p "You have issue with custom - if you want mapping to be configurable please contact me #{Gem.loaded_specs["jira_issues"].metadata.fetch('source_code_uri')}"
+      raise e
     end
   end
 end
