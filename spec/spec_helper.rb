a6c8e535ef2c9eadcba0128730ff06a30c51e4ca
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2017-12-30 10:36:50 +0000

    adding codecove.io

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
-require 'coveralls'
 require 'rspec'
 require 'jira_issues'
+
+require 'coveralls'
 Coveralls.wear!
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
